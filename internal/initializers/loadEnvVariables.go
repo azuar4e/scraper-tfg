4e4ff63d04bf0cef1abe46df751bb90d2ba15ffc
Angel <angelazuara444@gmail.com>
2026-03-26 23:15:40 +0100

    loadenv

--- internal/initializers/loadEnvVariables.go
+++ internal/initializers/loadEnvVariables.go
@@ -1,16 +1,16 @@
 package initializers
 
 import (
-	"log"
-
 	"github.com/joho/godotenv"
 )
 
+// esto solo sirve en local que recibe el archivo .env
+// en eks se le pasaran las variables a traves de un config map
+// y seran directamente accesibles haciendo os.Getenv("var")
 func LoadEnvVariables() {
-	err := godotenv.Load()
+	_ = godotenv.Load()
 
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-	log.Println("[+] Environment variables loaded")
+	// if err != nil {
+	// 	log.Fatal("Error loading .env file")
+	// }
 }
